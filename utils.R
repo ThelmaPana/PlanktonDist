0889217344bc9b8385802f9b61439536748b4d90
Thelma Panaiotis <thelma@crans.org>
2024-01-03 16:31:18 +0000

    Set utils

---
+++ utils.R
@@ -0,0 +1,13 @@
+## Packages ----
+#--------------------------------------------------------------------------#
+library(tidyverse)
+
+# Reading data
+library(arrow)
+
+# Spatial point pattern
+library(spatstat)
+
+
+theme_set(theme_minimal())
+
