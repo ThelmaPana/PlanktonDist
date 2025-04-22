adb1ca1c5d67e70aff5e57903752e5e8613fa2e6
ThelmaPana <thelma@crans.org>
2025-04-22 15:42:00 +0100

    Apply distance threshold for null Acantharea

--- 10a.null_acantharea_prep.R
+++ 10a.null_acantharea_prep.R
@@ -15,20 +15,20 @@ set.seed(seed)
 message("Reading data")
 
 # Correct image volume for x-axis
-load("data/01.corr_factor.Rdata")
+load("data/01a.corr_factor.Rdata")
 vol$x <- vol$x * med_corr
 
 sub_sample <- FALSE
 ## Subsampling
 if (sub_sample){
   load("data/00.images_sub.Rdata")
-  load("data/01.x_corrected_plankton_sub.Rdata")
+  load("data/01a.x_corrected_plankton_sub.Rdata")
   images <- images_sub
   plankton <- plankton_sub
 } else {
   ## All data
   images <- read_parquet("data/00.images_clean.parquet")
-  plankton <- read_parquet("data/01.x_corrected_plankton_clean.parquet")
+  plankton <- read_parquet("data/01a.x_corrected_plankton_clean.parquet")
 }
 
 
@@ -84,26 +84,35 @@ rand_points <- lapply(1:n_sets, function(i_set) {
 ## Compute distances and compare ----
 #--------------------------------------------------------------------------#
 message("Computing distances")
-dist_rand <- lapply(rand_points, compute_all_dist)
+
+dist_rand <- lapply(1:n_sets, function(i) {
+  compute_all_dist(rand_points[[i]]) %>% 
+    mutate(dist = dist * 51 / 10000) %>% # convert from px to cm
+    mutate(set = as.character(i)) # flag set
+}) %>% 
+  bind_rows()
+
+# Apply distance threshold
+dist_rand <- dist_rand |> filter(dist < dist_thr)
 
 message("Performing comparison of subsets")
 # Size of subsets
-dist_count <- dist_rand[[1]] %>% count(img_name) %>% mutate(n_cum = cumsum(n))
+dist_count <- dist_rand |> filter(set == "1") %>% count(img_name) %>% mutate(n_cum = cumsum(n))
 
 
 # Pair of sets to compare
 set_pairs <- crossing(set_a = 1:n_sets, set_b = 1:n_sets) %>% filter(set_a < set_b)
 
 # Loop on pairs of sets and perform kuiper test on each subset
-f_val_acant_dist <- pbmclapply(1:nrow(set_pairs), function(i) {
+null_ks_n_dist_acant <- pbmclapply(1:nrow(set_pairs), function(i) {
   message(paste0("Processing pair ", i, " out of ", nrow(set_pairs)))
   
   # Get sets of interest
   i_set_a <- set_pairs %>% slice(i) %>% pull(set_a)
   i_set_b <- set_pairs %>% slice(i) %>% pull(set_b)
-  set_a <- dist_rand[[i_set_a]]
-  set_b <- dist_rand[[i_set_b]]
-    
+  set_a <- dist_rand |> filter(set == as.character(i_set_a))
+  set_b <- dist_rand |> filter(set == as.character(i_set_b))
+  
   # Subsample to 10-000 quantiles if needed
   if (nrow(set_a) > 10000) {
     probs <- seq(0, 1, length.out = 10000)
@@ -113,28 +122,28 @@ f_val_acant_dist <- pbmclapply(1:nrow(set_pairs), function(i) {
     dist_set_a <- set_a$dist
     dist_set_b <- set_b$dist
   }
-    
+  
   # Perform kuiper test between sets
   kt <- kuiper_test(dist_set_a, dist_set_b)
   
   # Return results
   tibble(
     n_dist = nrow(set_a),
-    test_stat = kt[1],
+    kuiper_stat = kt[1],
     set_a = as.factor(i_set_a),
     set_b = as.factor(i_set_b)
   )
 }, mc.cores = n_cores, ignore.interactive = TRUE) %>% 
   bind_rows()
 
-# Compute log10 of n_dist and test_stat
-f_val_acant_dist <- f_val_acant_dist %>% 
+# Compute log10 of n_dist and kuiper_stat
+null_ks_n_dist_acant <- null_ks_n_dist_acant %>% 
   mutate(
     log_n_dist = log10(n_dist),
-    log_test_stat = log10(test_stat)
+    log_kuiper_stat = log10(kuiper_stat)
   )
-  
+
 
 ## Save ----
 #--------------------------------------------------------------------------#
-save(f_val_acant_dist, file = "data/07.f_val_acant_dist.Rdata")
+save(null_ks_n_dist_acant, file = "data/10a.null_ks_n_dist_acant.Rdata")
